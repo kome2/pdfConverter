998b6f2a1afa7ef1c94acb5b45812aa8527b7faa
kome2 <kome@hongo.wide.ad.jp>
2019-07-15 13:19:59 +0900

    enable to download startlist pdf(daily separate but session unseparate)

--- pdfgetter.py
+++ pdfgetter.py
@@ -7,82 +7,51 @@ import multiprocessing
 import threading
 
 ## edit here!!!
-urlHeader = "http://www.omegatiming.com/Competition?id=000111010AFFFFFFFFFFFFFFFFFFFFFF&day="
-
+gameUrl = "http://www.omegatiming.com/2019/tyr-pro-swim-series-5-clovis-live-results"
 
 def download(url, title):
+    # string url: download target url
+    # string title: saved file name
     print(title, ": downloading!")
     urllib.request.urlretrieve(url, "{0}".format(title))
     print(title, ": success!")
 
-def pdfextract(i, fp):
-    style = ""
+def pdfextract(fp):
+    if len(fp) < 5:
+        return
+    styleData = fp.find(class_="round").string # Women's Freestyle 800m Final
+
+    urls = fp.find_all(class_="two")
+    stUrl = urls[0].a.get("href") # startlist Url
+    reUrl = urls[1].a.get("href") # result url
     prefix = "http://www.omegatiming.com"
-    for j in i.find_all(class_="fileTitle"):
-        if "Men" in j.string:
-            style = "Men_" + j.string.split(" ")[1] + "_" + j.string.split(" ")[2]
-        elif "Women" in j.string:
-            style = "Women_" + j.string.split(" ")[1] + "_" + j.string.split(" ")[2]
-        elif "Mixed" in j.string:
-            style = "Mixed_" + j.string.split(" ")[1] + "_" + j.string.split(" ")[2]
-        else:
-            style += "_" + j.string
-    style = fp + style.replace(" ", "_") + ".pdf"
-    pdfurl = prefix + i.find(class_="fileName").a.get("href")
-    download(pdfurl, style)
+    stArray = styleData.split(" ")
+    if stArray[0] == "Men's":
+        stArray[0] = "Men"
+    elif stArray[0] == "Women's":
+        stArray[0] = "Women"
+    fname = stArray[0] + stArray[2] + stArray[1] + stArray[3] + ".pdf"
+
+    download(prefix + stUrl, fname)
 
 if __name__ == "__main__":
     day = sys.argv[1]
     session = sys.argv[2]
-    dailyUrl = urlHeader + day
-    r = requests.get(dailyUrl)
-
-    soup = BeautifulSoup(r.text, "lxml")
-    morning = False
-     
-    contents = soup.find(id='CompetitionContent')
-    index = 1
-    evening = False
-    styles = []
-
-    for i in contents.find_all_next("div"):
-        # if morning
-        if session == "m":
-            if i.find(class_="SessionSW") != None and morning == False:
-                # morning
-                morning = True
-            elif i.find(class_="SessionSW") == None and morning == True:
-                # between morning and evening
-                if "fileSW" in str(i):
-                    filePrefix = "./output/pdf/day" + day + session + "/" + day + session + "_" + str(index).zfill(2) + "_"
-                    styles += [[i, filePrefix]]
-                    index += 1
-            elif i.find(class_="SessionSW") != None and morning == True:
-                # evening
-                break
-            else:
-                # other
-                continue
-        elif session == "e":
-            if i.find(class_="SessionSW") != None and morning == False:
-                # morning
-                morning = True
-            elif i.find(class_="SessionSW") == None and morning == True and evening == False:
-                # between morning and evening
-                continue
-            elif i.find(class_="SessionSW") != None and morning == True:
-                # evening
-                evening = True
-            elif i.find(class_="SessionSW") == None and evening == True and evening == True:
-                # evening session
-                if "fileSW" in str(i):
-                    filePrefix = "./output/pdf/day" + day + session + "/" + day + session + "_" + str(index).zfill(2) + "_"
-                    styles += [[i, filePrefix]]
-                    index += 1
-            else:
-                # other
-                continue
+    r = requests.get(gameUrl)
     
-    for st in styles:
-        p = threading.Thread(target=pdfextract, args=(st))
-        p.start()
+    soup = BeautifulSoup(r.text, "lxml")
+     
+    contents = soup.find(class_='results-detail-page')
+    dayCount = int(day)
+
+    for div in contents.find_all_next(class_="block-table"):
+        # 指定した日付の抽出
+        dayCount -= 1
+        if dayCount != 0:
+            continue
+
+        # 種目ブロックの抽出
+        # class = rowごとに種目セットになっている
+        for row in div.find_all(class_="row"):
+            p = threading.Thread(target=pdfextract, args=([row]))
+            p.start()
